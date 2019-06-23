c800fd4e9ae13e0d25fea56ac612eac2d0a92235
Michal <michalk.rejowski@gmail.com>
2019-06-23 12:16:49 +0200

    Wersja z mapą

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        MyFileReader reader = new MyFileReader();
-        List<String> list = reader.readTextFile("Data.properties");
+
+        List<String> list = MyFileReader.readTextFile("Data.properties");
+        Map<String,String> map = MyFileReader.readFile(list);
 
 
-        System.out.println(MyFileReader.readOnlyGoodStuff(list));
+        System.out.println(map.keySet() + "" + map.values());
 
     }
 }
--- src/reader/MyFileReader.java
+++ src/reader/MyFileReader.java
@@ -1,21 +1,22 @@
 package reader;
 
 
+import com.sun.jdi.Value;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class MyFileReader {
-
-   public  static List<String> readTextFile(String filename)throws IOException {
-       List<String> lines = Files.readAllLines(Path.of(filename));
+/*
+    public static List<String> readTextFile(String filename) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of(filename));
 
         return lines;
-    }
-
+    }*/
+/*
    public static List<String> readOnlyGoodStuff(List<String> list)throws IOException {
        List<String> lines = new ArrayList<>();
        for ( String line : list){
@@ -26,6 +27,29 @@ public class MyFileReader {
 
        }
        return lines;
-   }
-}
+   }*/
 
+    public static List<String> readTextFile(String filename) throws IOException {
+        return Files.readAllLines(Path.of(filename));
+
+
+    }
+
+
+    public static Map<String,String> readFile(List<String> filename)  {
+
+        Map<String,String> result = new HashMap<>();
+        for ( String line : filename){
+            int indexOfEqualitySign = line.indexOf('=');
+            String propertyKey = line.substring(0, indexOfEqualitySign);
+            String propertyValue = line.substring(indexOfEqualitySign + 1);
+            result.put(propertyKey,propertyValue);
+        }
+
+
+        return result;
+        }
+
+
+
+}
